51bb1c443379655287c8fef23dc00b499d923229
Rajkumar-Khatua <rajkhatua2020@gmail.com>
2023-11-08 11:12:26 +0530

    Commit for deployment branch3

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:5500/api/",
+  baseURL: "https://gig-hub-server.vercel.app/api",
   withCredentials: true,
 });
 
